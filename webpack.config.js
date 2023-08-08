d225df6c1e17c09177f3a5dadd5138acc6b016da
paulpladziewicz <ppladziewicz@gmail.com>
2023-08-08 10:05:28 -0400

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV == 'production'
+const isProduction = process.env.NODE_ENV === 'production'
 
 const config = {
     entry: './src/index.tsx',
